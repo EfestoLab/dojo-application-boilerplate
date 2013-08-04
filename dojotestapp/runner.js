7e3c3b35f9c98ae756f29501dae93c760c87d64b
hdd <lorenzo.angeli@gmail.com>
2013-08-04 11:39:21 +0100

    move to 1.8.3

--- dojotestapp/runner.js
+++ dojotestapp/runner.js
@@ -3,9 +3,9 @@ require({
     baseUrl: '',
     // Defines the packages to be loaded, and map them into a unique namespace
     packages: [
-        { name: 'dojo', location: 'javascript/dojo/release-1.7.2/dojo'},
-        { name: 'dijit', location: 'javascript/dojo/release-1.7.2/dijit'},
-        { name: 'dojox', location: 'javascript/dojo/release-1.7.2/dojox'},
+        { name: 'dojo', location: 'javascript/dojo/release-1.8.3/dojo'},
+        { name: 'dijit', location: 'javascript/dojo/release-1.8.3/dijit'},
+        { name: 'dojox', location: 'javascript/dojo/release-1.8.3/dojox'},
         // Dgrid modules
         { name: 'dgrid', location: 'javascript/dgrid'},
         { name: 'xstyle', location: 'javascript/xstyle'},
