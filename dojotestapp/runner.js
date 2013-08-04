1e69dc6adba0455b503f347b74445b00bbec1db4
hdd <lorenzo.angeli@gmail.com>
2013-08-04 11:33:41 +0100

    move third party code in subfolder

--- dojotestapp/runner.js
+++ dojotestapp/runner.js
@@ -3,10 +3,14 @@ require({
     baseUrl: '',
     // Defines the packages to be loaded, and map them into a unique namespace
     packages: [
-        { name: 'dojo', location: 'dojo/release-1.7.2/dojo'},
-        { name: 'dijit', location: 'dojo/release-1.7.2/dijit'},
-        { name: 'dojox', location: 'dojo/release-1.7.2/dojox'},
-        { name: 'dgrid', location: 'dgrid'},
+        { name: 'dojo', location: 'javascript/dojo/release-1.7.2/dojo'},
+        { name: 'dijit', location: 'javascript/dojo/release-1.7.2/dijit'},
+        { name: 'dojox', location: 'javascript/dojo/release-1.7.2/dojox'},
+        // Dgrid modules
+        { name: 'dgrid', location: 'javascript/dgrid'},
+        { name: 'xstyle', location: 'javascript/xstyle'},
+        { name: 'put-selector', location: 'javascript/put-selector'},
+        // the application
         { name: 'dojotestapp', location: 'dojotestapp'}
     ],
 }, 
