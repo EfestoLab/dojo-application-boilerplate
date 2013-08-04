8ae86a2f0ee33906cc0f7073748c85e0075de9a6
hdd <lorenzo.angeli@gmail.com>
2013-08-04 14:31:56 +0100

    move to 1.9.1

--- dojotestapp/runner.js
+++ dojotestapp/runner.js
@@ -3,9 +3,9 @@ require({
     baseUrl: '',
     // Defines the packages to be loaded, and map them into a unique namespace
     packages: [
-        { name: 'dojo', location: 'javascript/dojo/release-1.8.3/dojo'},
-        { name: 'dijit', location: 'javascript/dojo/release-1.8.3/dijit'},
-        { name: 'dojox', location: 'javascript/dojo/release-1.8.3/dojox'},
+        { name: 'dojo', location: 'javascript/dojo/release-1.9.1/dojo'},
+        { name: 'dijit', location: 'javascript/dojo/release-1.9.1/dijit'},
+        { name: 'dojox', location: 'javascript/dojo/release-1.9.1/dojox'},
         // Dgrid modules
         { name: 'dgrid', location: 'javascript/dgrid'},
         { name: 'xstyle', location: 'javascript/xstyle'},
--- dojotestapp/widget/my_dGrid.js
+++ dojotestapp/widget/my_dGrid.js
@@ -5,7 +5,7 @@ define(['dojo/_base/declare',
         // This is required to make the dGrid parentable with dijit widget out of the box.
         'dgrid/extensions/DijitRegistry',],
     function(declare, OnDemandGrid, Selection, Keyboard, DijitRegistry){
-    return declare('my_dGrid',[OnDemandGrid, Selection, DijitRegistry],{    
+    return declare('my_dGrid',[OnDemandGrid, Selection, Keyboard, DijitRegistry],{    
         selectionMode: 'single',
     });
 });
